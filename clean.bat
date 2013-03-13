e4e4a0178029b36e88a353a91c1f58c15617b907
Eduardo Moranchel <emoranchel@gmail.com>
2013-03-13 16:59:02 -0600

    added build scripts and bin distributions

---
+++ clean.bat
@@ -0,0 +1,18 @@
+cd ImageCrawlerCore
+call mvn clean
+cd ..
+cd ImageCrawlerSwingView
+call mvn clean
+cd ..
+cd konachanator
+call mvn clean
+cd ..
+cd sankakunator
+call mvn clean
+cd ..
+cd konachanatorSwingLauncher
+call mvn clean
+cd ..
+cd sankakunatorSwingLaunch
+call mvn clean
+cd ..
