f66d4859a9ca117f3d186de0fc1171ea7897e7ba
Eduardo Moranchel <emoranchel@gmail.com>
2013-03-26 20:50:13 -0600

    optimized file checking and already downloaded

--- ImageCrawlerCore/src/main/java/com/imagecrawl/tasks/DefaultCheckTask.java
+++ ImageCrawlerCore/src/main/java/com/imagecrawl/tasks/DefaultCheckTask.java
@@ -3,7 +3,18 @@ package com.imagecrawl.tasks;
 import com.imagecrawl.api.CrawlerMessage;
 import com.imagecrawl.model.GalleryImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import org.asmatron.messengine.ControlEngine;
@@ -11,33 +22,60 @@ import org.asmatron.messengine.MessEngine;
 
 public final class DefaultCheckTask extends CheckTask {
 
-  public DefaultCheckTask(ExecutorService executorService, MessEngine messEngine, ControlEngine controlEngine, CrawlerMessage<GalleryImage> crawlerMessage) {
-    super(executorService, messEngine, controlEngine, crawlerMessage);
-  }
+    private final static Map<String, Set<Integer>> checkers = new HashMap<>();
 
-  @Override
-  protected boolean fileExist() {
-    File file = new File(action.getSavePath());
-    Set<Integer> filesFound = new HashSet<>();
-    addFiles(file, filesFound);
-    return filesFound.contains(value.getId());
-  }
-
-  private void addFiles(File dir, Set<Integer> filesFound) {
-    if (dir.exists()) {
-      for (File file : dir.listFiles()) {
-        if (file.isDirectory()) {
-          addFiles(file, filesFound);
-        } else {
-          String name = file.getName();
-          try {
-            String idstr = name.substring(0, name.indexOf('.'));
-            int id = Integer.parseInt(idstr);
-            filesFound.add(id);
-          } catch (Exception e) {
-          }
-        }
-      }
+    public DefaultCheckTask(ExecutorService executorService, MessEngine messEngine, ControlEngine controlEngine, CrawlerMessage<GalleryImage> crawlerMessage) {
+        super(executorService, messEngine, controlEngine, crawlerMessage);
     }
-  }
+
+    @Override
+    protected boolean fileExist() {
+        synchronized (checkers) {
+            Set<Integer> ids = checkers.get(action.getSavePath());
+            if (ids == null) {
+                ids = new HashSet<>();
+                addFiles(ids);
+                addDownloads(ids);
+                checkers.put(action.getSavePath(), ids);
+            }
+            boolean exist = ids.contains(value.getId());
+            ids.add(value.getId());
+            return exist;
+        }
+    }
+
+    private void addFiles(final Set<Integer> ids) {
+        try {
+            File file = new File(action.getSavePath());
+            Files.walkFileTree(Paths.get(file.toURI()), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    String name = file.toFile().getName();
+                    try {
+                        String idstr = name.substring(0, name.indexOf('.'));
+                        int id = Integer.parseInt(idstr);
+                        ids.add(id);
+                    } catch (Exception e) {
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addDownloads(Set<Integer> ids) {
+        File dir = new File(action.getSavePath());
+        try (FileInputStream input = new FileInputStream(new File(dir, "downloads.data"))) {
+            byte[] buffer = new byte[4];
+            ByteBuffer bb = ByteBuffer.wrap(buffer);
+            while (input.read(buffer) > 0) {
+                ids.add(bb.getInt());
+                bb.rewind();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
