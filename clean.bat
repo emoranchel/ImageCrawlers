137b601b837d2229b87c95baf633a904c72aacc7
Eduardo Moranchel <emoranchel@gmail.com>
2013-03-26 20:55:53 -0600

    Updating build scripts

--- clean.bat
+++ clean.bat
@@ -1,27 +1,18 @@
 cd ImageCrawlerCore
 call mvn clean
 cd ..
-cd ImageCrawlerSwingView
+cd ImageCrawlerFxView
 call mvn clean
 cd ..
-cd ImageCrawlerFxView
+cd ImageCrawlerLauncher
+call mvn clean
+cd ..
+cd ImageCrawlerSwingView
 call mvn clean
 cd ..
 cd konachanator
 call mvn clean
 cd ..
 cd sankakunator
-call mvn clean
-cd ..
-cd konachanatorSwingLauncher
-call mvn clean
-cd ..
-cd sankakunatorSwingLauncher
-call mvn clean
-cd ..
-cd konachanatorFxLauncher
-call mvn clean
-cd ..
-cd sankakunatorFxLauncher
 call mvn clean
 cd ..
