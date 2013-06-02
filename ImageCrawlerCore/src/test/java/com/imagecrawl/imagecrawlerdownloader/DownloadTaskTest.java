2a2fe442a723ac91c5c0c93faafbd9050da8ae76
Eduardo Moranchel <emoranchel@gmail.com>
2013-06-01 20:04:23 -0500

    Fixed illegal files bug

--- ImageCrawlerCore/src/test/java/com/imagecrawl/imagecrawlerdownloader/DownloadTaskTest.java
+++ ImageCrawlerCore/src/test/java/com/imagecrawl/imagecrawlerdownloader/DownloadTaskTest.java
@@ -9,9 +9,26 @@ public class DownloadTaskTest {
     @Test
     public void testPaths() {
         assertEquals(
-                "/D:/pictures/konachan/Questionable/156229.desu_yo.jpg",
+                "/D:/pictures/konachan/Questionable/156229.desu_yo.jpg", //
                 DownloadTask.getValidFileName(
-                "/D:/pictures/konachan/Questionable/156229.desu_yo?.jpg"
+                "/D:/pictures/konachan/Questionable/156229.desu_yo?.jpg" //
                 ));
+
+        assertEquals(
+                "D:/pictures/danbooru/Safe/1431199.k-on!.akiyama_mio.hirasawa_yui.kotobuki_tsumugi.tainaka_ritsu.2girls.4girls.black_eyes.black_hair.blonde_hair.blue_eyes.blush.brown_eyes.brown_hair.long_hair.lowres.multiple_girls.jpg", //
+                DownloadTask.getValidFileName(
+                "D:/pictures/danbooru/Safe/1431199.k-on!.akiyama_mio.hirasawa_yui.kotobuki_tsumugi.tainaka_ritsu.2girls.4girls.black_eyes.black_hair.blonde_hair.blue_eyes.blush.brown_eyes.brown_hair.long_hair.lowres.multiple_girls.jpg" //
+                ));
+        assertEquals(
+                "D:/pictures/danbooru/Safe/1431227.idolmaster.idolmaster_cinderella_girls.imai_kana.mokufuu.1girl", //
+                DownloadTask.getValidFileName(
+                "D:/pictures/danbooru/Safe/1431227.idolmaster.idolmaster_cinderella_girls.imai_kana.mokufuu.1girl." //
+                ));
+        assertEquals(
+                "D:/pictures/danbooru/Safe/1431227.idolmaster.idolmaster_cinderella_girls.imai_kana.mokufuu.1girl.jpg", //
+                DownloadTask.getValidFileName(
+                "D:/pictures/danbooru/Safe/1431227.idolmaster.idolmaster_cinderella_girls.imai_kana.mokufuu.1girl..jpg" //
+                ));
+
     }
 }
