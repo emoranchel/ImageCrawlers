ba370f3a9c614e2fb476bdc8355a3fa571f1a8f0
Eduardo Moranchel <emoranchel@gmail.com>
2014-05-26 19:55:55 -0500

    updating fx app, moving stuff... i guess

--- ImageCrawlerLauncher/src/main/java/com/imagecrawl/launcher/App.java
+++ ImageCrawlerLauncher/src/main/java/com/imagecrawl/launcher/App.java
@@ -10,7 +10,7 @@ import org.asmatron.messengine.engines.Engine;
 public class App {
 
   public static void main(String[] args) throws Exception {
-    if (containArg(args, "-fx")) {
+     if (containArg(args, "-fx")) {
       FxApplication.main(args);
     } else if (containArg(args, "-swing")) {
       SwingApplication.main(args);
@@ -26,21 +26,5 @@ public class App {
       }
     }
     return false;
-  }
-
-  public static void setupFactory(String[] args, Engine engine) {
-    if (containArg(args, "konachan")) {
-      engine.set(API.Model.FACTORY, new KonachanCrawlerFactory(engine, engine), null);
-      engine.set(API.Model.TITLE, "Konachan.com", null);
-    } else if (containArg(args, "sankaku")) {
-      engine.set(API.Model.FACTORY, new SankakuCrawlerFactory(engine, engine), null);
-      engine.set(API.Model.TITLE, "SankakuComplex.com", null);
-    } else if (containArg(args, "danbooru")) {
-      engine.set(API.Model.FACTORY, new DanbooruCrawlerFactory(engine, engine), null);
-      engine.set(API.Model.TITLE, "Danbooru.donmai.us", null);
-    } else if (containArg(args, "sankakuTop")) {
-      engine.set(API.Model.FACTORY, new SankakuTopCrawlerFactory(engine, engine), null);
-      engine.set(API.Model.TITLE, "SankakuTop.com", null);
-    }
   }
 }
